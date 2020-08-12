0ab2efccd74e5e745fc904822ea9eca231be93f0
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-08-12 10:34:20 +0900

    Fix typo

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,6 @@
 function adustTextbox(obj) {
   obj.style.height = "80px";
-  const wSclollHeight = parseInt(obj.scrollHeight);
+  const scrollHeight = parseInt(obj.scrollHeight);
 
-  obj.style.height = wSclollHeight + "px";
+  obj.style.height = scrollHeight + "px";
 }
