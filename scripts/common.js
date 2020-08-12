e90399243d48e03053c11fb0da7dd6a70513d528
eve0415 <40632436+eve0415@users.noreply.github.com>
2020-08-12 11:39:29 +0900

    Change height synchronously with textbox

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,10 @@
 function adustTextbox(obj) {
+  const content = document.getElementsByClassName('content')[0];
+  
   obj.style.height = "80px";
   const scrollHeight = parseInt(obj.scrollHeight);
 
   obj.style.height = scrollHeight + "px";
+  
+  content.scrollHeight = content.scrollHeight + scrollHeight;
 }
