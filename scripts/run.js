5500c5091ce2edf1e8608d4a84d3b9c67be5266b
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-27 19:40:47 +0900

    Update run.js

--- scripts/run.js
+++ scripts/run.js
@@ -18,6 +18,26 @@ $("#btn1").on("click", async function() {
     .then(() => adjustResultBox());
 });
 
+$("#Node-btn1").on("click", async function() {
+  const code = $("textarea[name='Node-code']").val();
+  fetch("https://run-lan.herokuapp.com/Node", {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
+    },
+    body: code
+  })
+    .then(res => res.json())
+    .then(res => {
+      if(res.error){
+        $("#result").val(res.error).addClass("result-content-error")
+      }else{
+        $("#result").val(res.result).removeClass("result-content-error")
+      }
+    })
+    .then(() => adjustResultBox());
+});
+
 const adjustResultBox = () => {
   const content = document.getElementsByClassName('content')[0];
   const result = document.getElementById('result');
