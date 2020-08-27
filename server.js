ed82e43cd2aac9a10492abe8e3d3551982240121
Mikan-OwO <68114595+Mikan-OwO@users.noreply.github.com>
2020-08-27 19:45:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { VM } = require("vm2");
+const { NodeVM } = require("vm2");
 const cors = require("cors");
 
 const app = express();
@@ -22,6 +23,34 @@ app.listen(port, () => {
 app.post("/js", async (req, res) => {
   const code = req.body;
   const vm = new VM({ timeout: 5000 });
+  
+  res.append("content-type", "application/json");
+  
+  try {
+    const result = await vm.run(code);
+    res.send({ result: String(result) });
+  } catch (error) {
+    res.send({ error: String(error) });
+  }
+});
+
+app.post("/Node", async (req, res) => {
+  const code = req.body;
+  const vm = new NodeVM({
+    timeout: 5000,
+    console: 'inherit',
+    sandbox: {},
+    require: {
+    external: true,
+    builtin: ['fs', 'path'],
+    root: "./",
+    mock: {
+     fs: {
+      readFileSync() { return 'Nice try!'; }
+      }
+     }
+    }
+   });
   
   res.append("content-type", "application/json");
   
